b4be976d698e089a924a81a92b436b6c00541a50
Ibrahim <111103567+iibarhimoo@users.noreply.github.com>
2025-07-22 19:09:14 +0300

    Update AuthService.java

--- src/main/java/com/chat/service/AuthService.java
+++ src/main/java/com/chat/service/AuthService.java
@@ -8,13 +8,13 @@ public class AuthService {
 
     static {
         // Preloaded users (in production, use database)
-        userDatabase.put("Yahya", "networkProgramming");
-        userDatabase.put("Ibrahim", "networkProgramming");
-        userDatabase.put("Hasan" , "networkProgramming");
-        userDatabase.put("Faisal" , "networkProgramming");
-        userDatabase.put("Safwan" , "networkProgramming");
-        userDatabase.put("Nouruladeen" , "networkProgramming");
-        userDatabase.put("Saud" , "networkProgramming");
+        userDatabase.put("Yahya", "12345");
+        userDatabase.put("Ibrahim", "12345");
+        userDatabase.put("Hasan" , "12345");
+        userDatabase.put("Faisal" , "12345");
+        userDatabase.put("Safwan" , "12345");
+        userDatabase.put("Nouruladeen" , "12345");
+        userDatabase.put("Saud" , "12345");
     }
 
     public static boolean authenticate(String username, String password) {
